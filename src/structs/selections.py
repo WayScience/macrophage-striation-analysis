5a7782ea98a3bebfc1ee3d662b44478bf3a6bf9a
axiomcura <erikishere3@gmail.com>
2022-12-12 09:41:19 -0700

    documentation and attribute changes

--- src/structs/selections.py
+++ src/structs/selections.py
@@ -5,8 +5,23 @@ from typing import Tuple, Union
 @dataclass(slots=True)
 class ImageCropSelection:
     """Data structure containing metadata information of the selected
-    cropped images. These are bounding boxes"""
+    cropped images.
 
-    img_id: int
-    file_name: str
+    Parameters: 
+    -----------
+    crop_id : int
+        unique id of cropped image data:
+    file_source : str
+        Where the cropped image was taken from
+    crop_position: Tuple[int, int, int, int]
+        Contains the left, top, right and bottom positional values
+
+    Returns
+    -------
+    Self
+        ImageCropSelection, dataclass that contains metadata of the
+        cropped image.
+    """
+    crop_id: int
+    file_source: str
     crop_position: Tuple[int, int, int, int]

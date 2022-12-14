36f6b7d5dcd5bc1a76b2d2bab0c7b31739a43fd8
axiomcura <erikishere3@gmail.com>
2022-12-14 15:52:27 -0800

    added functions to modules

--- src/common/_io.py
+++ src/common/_io.py
@@ -8,12 +8,63 @@ import json
 from pathlib import Path
 from typing import List, Optional
 
+from PIL import Image
+
 from src.common.errors import NotCoordArrayError
 from src.guards.arrays_guards import is_array_of_coords
 from src.structs.selections import ImageCropSelection
 
 
-# TODO: must finish later
+def load_tiff_images(
+    img_dir: str | Path,
+) -> List[Image.Image]:
+    """Loads images from given directory. Returns a list object
+
+    Parameters
+    ----------
+    img_dir : str | Path
+       directory path containing image paths
+
+    Returns
+    -------
+    List[PIL.TiffImagePlugin.TiffImageFile]
+        List of tuples that contains the image path and the PIL image object
+
+    Raises
+    ------
+    FileNotFoundError
+        Raised if the 'img_dir' does not exist
+    TypeError
+        Raised if images are not in `tiff` format
+        Raised if `img_dir` is not str or Path type
+    """
+
+    # type checking
+    # -- if str type, convert to Path type
+    accepted_types = (str, Path)
+    if not isinstance(img_dir, accepted_types):
+        _type = type(img_dir)
+        raise TypeError(f"'fpath' must str or path type not {_type}")
+    if isinstance(img_dir, str):
+        img_dir = Path(img_dir)
+    if not img_dir.exists():
+        raise FileNotFoundError(f"Unable to find: {str(img_dir)}")
+    if img_dir.suffix == "tiff":
+        raise TypeError(f"Image must be .tiff format not {img_dir.suffix}")
+
+    # grab all .tiff images within given image directory
+    tiff_paths = img_dir.glob("*.tiff")
+
+    # loading images into memory
+    # -- List[Tuple(path/to/image, PIL Image Object)]
+    loaded_images = []
+    for tiff_path in tiff_paths:
+        tiff_img = Image.open(tiff_path)
+        loaded_images.append(tiff_img)
+
+    return loaded_images
+
+
 def coord_to_json(
     coord_array: List[ImageCropSelection],
     outname: Optional[str] = "coord_chart",

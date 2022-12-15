3ab1725ff69bb2ae1982977efb30ceebd95f55c8
axiomcura <erikishere3@gmail.com>
2022-12-15 10:55:39 -0800

    removed gui folder

--- src/gui/image_selector.py
+++
@@ -1,8 +0,0 @@
-"""
-module: image_selector.py
-
-Module contains all the instructions on how the user will interact with the
-simple gui interface when selected images.
-
-GUI will be written in tkinter
-"""

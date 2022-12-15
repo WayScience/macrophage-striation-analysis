1e1e67b90c67e55f15133b761e0ea59a9a3eeedb
axiomcura <erikishere3@gmail.com>
2022-12-15 10:47:01 -0800

    added array guard type

--- src/guards/arrays_guards.py
+++ src/guards/arrays_guards.py
@@ -4,15 +4,27 @@ module: arrays_guards.py
 Module that support strict type checking. This focuses on check the array types
 and elements within
 """
+from typing import List, Tuple, TypeGuard
+
 from src.structs.selections import ImageCropSelection
 
 
-def is_array_of_coords(arr: object) -> bool:
-    """Checks if the given object is an array of ImageCropSelection"""
+def is_list_of_crop_selections(
+    array: object,
+) -> TypeGuard[List[ImageCropSelection]]:
+    """Checks if the array contains only ImageCropSelection objects.
 
-    accepted_array_types = (tuple, list)
+    Returns
+    -------
+    TypeGuard[List[ImageCropSelection]]
+        Ensures that the list provided is all ImageCropSelection objects
+    """
+    # only accepts tuples or lists
+    accepted_types = (List, Tuple)
+
+    # checking for both array and element types
     return (
-        all(isinstance(elm, ImageCropSelection) for elm in arr)
-        if isinstance(arr, accepted_array_types)
+        all(isinstance(elm, ImageCropSelection) for elm in array)
+        if isinstance(array, accepted_types)
         else False
     )

aeeccd7eeeb236680a9d32b5f697e50cb44174e5
axiomcura <erikishere3@gmail.com>
2023-01-06 12:51:35 -0700

    added path obj test

--- src/testing/test_core.py
+++ src/testing/test_core.py
@@ -5,6 +5,7 @@ Testing module focuses on testing the core functions of the analysis pipeline.
 """
 
 import unittest
+from pathlib import Path
 from typing import Iterator
 
 from PIL import Image
@@ -23,6 +24,22 @@ class TestUtils(unittest.TestCase):
 
         # loading directory
         loaded_img = load_tiff_images("./test_data/tiff_images")
+
+        # checking if iterator is returned
+        self.assertIsInstance(loaded_img, Iterator)
+
+        # checking if elements are loaded as Image.Image types
+        images = list(loaded_img)
+        img_sel = images[0]
+        self.assertIsInstance(img_sel, Image.Image)
+
+    def test_load_tiff_img_path_obj(self) -> None:
+        """With a given path to a `.tiff` file, should return and Image.Image
+        object"""
+
+        # loading directory
+        path_obj = Path("./test_data/tiff_images")
+        loaded_img = load_tiff_images(path_obj)
 
         # checking if iterator is returned
         self.assertIsInstance(loaded_img, Iterator)

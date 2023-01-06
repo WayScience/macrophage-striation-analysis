5c8727d3cf1cd134543c99b1841460075e86e67e
Erik Serrano <31600622+axiomcura@users.noreply.github.com>
2023-01-06 10:55:14 -0700

    Update setup.py
    Co-authored-by: Gregory Way <gregory.way@gmail.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="macrophase-striation-analysis",
     version="0.0.1",
-    author="Erik Serrano, Way Lab",
+    author="Erik Serrano",
     author_email="erik.serrano@cuanschutz.edu",
     description="Analysis package identifying macrophage stration patterns",
     url="https://github.com/WayScience/macrophage-striation-analysis",

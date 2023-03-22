d23e3210e8241c70a912f1498bd3f3f6de661e77
axiomcura <erikishere3@gmail.com>
2023-03-22 15:11:51 -0600

    update classification function

--- src/ui/actions.py
+++ src/ui/actions.py
@@ -11,36 +11,49 @@ def load_images_to_ui():
     pass
 
 
-def update_image_classification(crop_image_metadata: dict, value_update: bool):
-    """Updates classification attribute with the cropped image metadata.
-    Classification
+def update_image_classification(
+    crop_image_metadata: dict, classification_label: bool
+) -> None:
+    """Updates classification attribute with the cropped image metadata found
+    within the cropped image metadata.
+
+    The function targets the `classification` attribute within the cropped
+    meta data and places a 0 or 1 as its value.
+
+    True or False booleans are converted to 1 or 0 integers, respectively
 
     Parameters
     ----------
     crop_image_metadata : dict
         cropped image metadata
 
-    value_update : bool
+    classification_label : bool
+        Update classification label to true or false indicating presence of
+        a specific
 
-
+    Returns
+    -------
+    None
+        Updated cropped metadata under the classification attribute.
     """
 
     # type checking
     if not isinstance(crop_image_metadata, dict):
         input_type = type(crop_image_metadata)
-        raise TypeError(f"cropped image data must be in dict format. not: {input_type}")
+        raise TypeError(
+            f"cropped image data must be in dict format. not: {input_type}"
+        )
 
-    if not isinstance(value_update, bool):
-        input_type = type(value_update)
-        raise TypeError(f"value_update must be either True or False, not {input_type}")
+    if not isinstance(classification_label, bool):
+        input_type = type(classification_label)
+        raise TypeError(
+            f"value_update must be either True or False, not {input_type}"
+        )
 
     # updating classification attribute
     try:
-        crop_image_metadata["classification"] = value_update
+        crop_image_metadata["classification"] = int(classification_label)
     except KeyError as e:
-        raise ConfigAttributeNotFound("classification attributes does not exist") from e
-
-
-# -----------------
-# private functions
-# -----------------
+        raise ConfigAttributeNotFound(
+            "classification attributes does not exist"
+        ) from e
